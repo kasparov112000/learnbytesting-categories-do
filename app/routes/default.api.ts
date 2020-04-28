c9e1cab7d5266de445169b3da6c333a8055a2322
hipolito nunez <hipolito.r.nunez@pwc.com>
2020-04-27 22:21:29 -0400

    Add /pingcategories endpoint

--- app/routes/default.api.ts
+++ app/routes/default.api.ts
@@ -1,6 +1,15 @@
+import { logger } from '@quicksuite/commons-logger';
+
 export default function (app, express, serviceobject) {
   let router = express.Router();
 
+  router.get('/pingcategories', (req, res) => {
+    logger.log('info', 'GET Ping Categories', {
+      timestamp: Date.now(),
+       txnId: req.id
+     });
+    res.status(200).json({ message: 'pong from categories' });
+  });
 
   router.post('/categories/:id', (req, res) => {
     serviceobject.getByLineOfService(req, res);
